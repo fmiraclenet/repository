138cb7d9bec492ce9f2cb6b4567dd18807450874
fmiraclenet <fmiracle.net@gmail.com>
2024-10-20 21:11:25 +0900

    confファイルの整理とコメントアウト削除

--- mysql/scripts/db_init.sh
+++ mysql/scripts/db_init.sh
@@ -9,9 +9,9 @@
 #MYSQL_PORT=3306
 
 # DB接続用のユーザファイルを作成する
-echo '[client]' > /tmp/my.conf
-echo 'user = ' ${MYSQL_ROOT_USER} >> /tmp/my.conf
-echo 'password = ' ${MYSQL_ROOT_PASSWORD} >> /tmp/my.conf
+#echo '[client]' > /tmp/my.conf
+#echo 'user = ' ${MYSQL_ROOT_USER} >> /tmp/my.conf
+#echo 'password = ' ${MYSQL_ROOT_PASSWORD} >> /tmp/my.conf
 
 # SHEMA作成
 #bash /tmp/scripts/createdb.sh wordpress

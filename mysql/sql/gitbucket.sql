28aabd23142f56959b18696ebf2162dd19cb1fd1
fmiraclenet <fmiracle.net@gmail.com>
2024-10-20 20:32:44 +0900

    mysqlのバージョンアップ対応

--- mysql/sql/gitbucket.sql
+++ mysql/sql/gitbucket.sql
@@ -1,4 +1,4 @@
 CREATE SCHEMA `gitbucket` ;
-CREATE USER 'gitbucket'@'%' IDENTIFIED WITH mysql_native_password BY 'gitbucket';
+CREATE USER 'gitbucket'@'%' IDENTIFIED WITH caching_sha2_password BY 'gitbucket';
 GRANT ALL PRIVILEGES ON *.* TO 'gitbucket'@'%';
 FLUSH PRIVILEGES;
